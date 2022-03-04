74dcf8acfe003951925c5862283795631aa29a45
Compey <hi@devcomp.tk>
2022-03-04 20:27:25 +0530

    attempt to patch

--- chat_welcomer.py
+++ chat_welcomer.py
@@ -5,14 +5,6 @@ import os
 client = discord.Client()
 
 @client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-
-@client.event
 async def on_message(message):
   if message.author == client.user:
     return
